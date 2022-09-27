35a1c71e7477c4e7a49301d1f142e959865c2db2
rawwr <akshayace0302@gmail.com>
2022-09-28 01:32:20 +0530

    function as a type

--- 02-basic-and-basic-types/index.js
+++ 02-basic-and-basic-types/index.js
@@ -7,7 +7,12 @@ var printResult = function (num) {
 printResult(addUp(5, 12));
 //undefined is a type in the ts
 // let someValue: undefined
-var printResult1 = function (num) {
-    console.log(num);
-    return;
-};
+// const printResult1 = (num: number): undefined => {
+//   console.log(num);
+//   return;
+// };
+var combineValues;
+combineValues = addUp;
+// combineValues = 67;
+// combineValues = printResult; //undefined
+console.log(combineValues(8, 8));
--- 02-basic-and-basic-types/index.ts
+++ 02-basic-and-basic-types/index.ts
@@ -11,7 +11,15 @@ printResult(addUp(5, 12));
 //undefined is a type in the ts
 // let someValue: undefined
 
-const printResult1 = (num: number): undefined => {
-  console.log(num);
-  return;
-};
+// const printResult1 = (num: number): undefined => {
+//   console.log(num);
+//   return;
+// };
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = addUp;
+// combineValues = 67;
+// combineValues = printResult; //undefined
+
+console.log(combineValues(8, 8));
