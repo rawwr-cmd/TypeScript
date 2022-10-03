143e0c0294092ab34ec693e7e1f7bdebaf71ecc8
rawwr <akshayace0302@gmail.com>
2022-10-03 10:41:55 +0530

    func overloads and optional chaining

--- 05-advanced-types/dist/func-overloads.js
+++ 05-advanced-types/dist/func-overloads.js
@@ -1,9 +1,16 @@
 "use strict";
-const add1 = (a, b) => {
+function add1(a, b) {
     if (typeof a === "string" || typeof b === "string") {
         return a.toString() + b.toString();
     }
     return a + b;
+}
+const result2 = add1(1, "rahul");
+const result1 = add1("rawwr", " gold");
+result1.split(" ");
+const fetchedUserData = {
+    id: "u1",
+    name: "rahul",
+    job: { title: "CEO", description: "My own company" },
 };
-const result1 = add(6, 8);
-console.log(result1);
+console.log(fetchedUserData?.job?.title);
--- 05-advanced-types/src/func-overloads.ts
+++ 05-advanced-types/src/func-overloads.ts
@@ -23,3 +23,16 @@ const result2 = add1(1, "rahul");
 const result1 = add1("rawwr", " gold");
 
 result1.split(" ");
+
+//optional chaining
+
+const fetchedUserData = {
+  id: "u1",
+  name: "rahul",
+  job: { title: "CEO", description: "My own company" },
+};
+
+// for javascript
+// console.log(fetchedUserData.job && fetchedUserData.job.title);
+
+console.log(fetchedUserData?.job?.title);
